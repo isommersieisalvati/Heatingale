6d6b36083aa28bd700fea2b5f4d84d5f03832e05
Fiona0229 <fiona2016@protonmail.com>
2025-03-22 20:58:30 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CanadianElection from "./components/CanadianElection";
+
+function App() {
+    return (
+        <>
+            <CanadianElection />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/CanadianElection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CanadianElection = () => {
+    return <div>CanadianElection</div>;
+};
+
+export default CanadianElection;
